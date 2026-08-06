216d286f8be54ca255368a4dcdf40d93bee0aca6
vivek831818-png <vivek831818@gmail.com>
2026-08-06 23:36:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const audio = document.getElementById("audio");
 const play = document.getElementById("play");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const cover = document.getElementById("cover");
+const songList = document.getElementById("songList");
 
 const CLIENT_ID = "a7a994c0";
 
@@ -11,18 +14,35 @@ let currentSong = 0;
 
 async function loadOnlineSongs() {
     try {
-        const res = await fetch(`https://api.jamendo.com/v3.0/tracks/?client_id=${CLIENT_ID}&format=json&limit=20`);
-        const data = await res.json();
+        const res = await fetch(
+            `https://api.jamendo.com/v3.0/tracks/?client_id=${CLIENT_ID}&format=json&limit=20`
+        );
 
+        const data = await res.json();
         songs = data.results;
+
+        songList.innerHTML = "";
+
+        songs.forEach((song, index) => {
+            songList.innerHTML += `
+                <div class="song" onclick="playSong(${index})">
+                    <img src="${song.image}">
+                    <div>
+                        <h3>${song.name}</h3>
+                        <p>${song.artist_name}</p>
+                    </div>
+                    <i class="fa-solid fa-play"></i>
+                </div>
+            `;
+        });
 
         if (songs.length > 0) {
             loadSong(0);
         }
 
-    } catch (e) {
-        console.log(e);
-        alert("Songs load nahi hue!");
+    } catch (err) {
+        console.log(err);
+        alert("Jamendo API se songs load nahi hue.");
     }
 }
 
@@ -35,6 +55,10 @@ function loadSong(index) {
     title.textContent = songs[index].name;
     artist.textContent = songs[index].artist_name;
     cover.src = songs[index].image;
+}function playSong(index) {
+    loadSong(index);
+    audio.play();
+    play.innerHTML = "⏸";
 }
 
 play.onclick = function () {
@@ -47,6 +71,26 @@ play.onclick = function () {
         audio.pause();
         play.innerHTML = "▶";
     }
+};
+
+next.onclick = function () {
+    currentSong++;
+    if (currentSong >= songs.length) {
+        currentSong = 0;
+    }
+    playSong(currentSong);
+};
+
+prev.onclick = function () {
+    currentSong--;
+    if (currentSong < 0) {
+        currentSong = songs.length - 1;
+    }
+    playSong(currentSong);
+};
+
+audio.onended = function () {
+    next.onclick();
 };
 
 loadOnlineSongs();
